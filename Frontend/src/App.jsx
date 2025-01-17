6e397e326b04ac3dbfbd1f1e8e2a9dd8d6b32279
mahanandhan <mahanandhanmanish@gmail.com>
2025-01-17 12:04:12 +0530

    added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LoginPage } from "./routes/routes";
+import { LoginPage, SignupPage } from "./Routes";
 import "./App.css";
 function App() {
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   )
--- Frontend/src/routes/routes.js
+++
@@ -1,3 +0,0 @@
-import LoginPage from "../Pages/LoginPages";
-import Login from "../Components/Auth/Login";
-    export{Login, LoginPage}
